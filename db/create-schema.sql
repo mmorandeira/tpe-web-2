054876172b7ec36afb3bb31f18cbb82e3f04e514
Matias Morandeira <morandeiramatias@gmail.com>
2022-10-14 16:03:37 -0300

    Initial tables and database created.

---
+++ db/create-schema.sql
@@ -0,0 +1,21 @@
+CREATE DATABASE IF NOT EXISTS app;
+
+CREATE TABLE IF NOT EXISTS app.category(
+    id INT NOT NULL AUTO_INCREMENT,
+    name VARCHAR(20) NOT NULL,
+    description VARCHAR(120),
+    color CHAR(6),
+    PRIMARY KEY(id)
+);
+
+CREATE TABLE IF NOT EXISTS app.expense(
+    id INT NOT NULL AUTO_INCREMENT,
+    date TIMESTAMP NOT NULL, 
+    product_name VARCHAR(60) NOT NULL,
+    cost DECIMAL (10, 2),
+    category_id INT NOT NULL,
+    PRIMARY KEY (id),
+    FOREIGN KEY (category_id) REFERENCES category(id)
+);
+
+
--- readme.md
+++ readme.md
@@ -1,3 +1,9 @@
 # TPE Web 2: Gestion de gastos
 
 <!--- TBC --->
+
+
+Para crear la base de datos y las tablas correr:
+```bash
+docker exec -i tpe-web2-db mysql < ./db/create-schema.sql
+```
