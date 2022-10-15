f4597bf27f0385fbea3be19d0c4b45e506a7d2a0
Matias Morandeira <morandeiramatias@gmail.com>
2022-10-15 16:02:05 -0300

    Script for initial table population added.

---
+++ db/hydrate-tables.sql
@@ -0,0 +1,26 @@
+
+INSERT INTO app.category (id, name, description, color) VALUES
+    (1, 'Supermercado', 'Gastos como leche, pan, articulos de limpieza, etc.', '00BB2D'),
+    (2, 'Gastos Fijos', 'Gastos de las tarjetas, deporte, servicios, impuestos, etc.', '00BB2D'),
+    (3, 'Salud', 'Gastos relacionados a la salud como farmacia, medicos, etc.', '00BB2D'),
+    (4, 'Ocio', 'Gastos como por ejemplo: juegos, salidas, etc.', '00BB2D'),
+    (5, 'Transporte', 'Gastos relacionados al transporte, nafta, colectivos, etc.', '00BB2D')
+;
+
+INSERT INTO app.expense (id, date, product_name, cost, category_id) VALUES
+    (1, '1998-01-23 12:45:56', '', 0.0, 1),
+    (2, '1998-01-23 12:45:56', '', 0.0, 1),
+    (3, '1998-01-23 12:45:56', '', 0.0, 1),
+    (4, '1998-01-23 12:45:56', '', 0.0, 1),
+    (5, '1998-01-23 12:45:56', '', 0.0, 1),
+    (6, '1998-01-23 12:45:56', '', 0.0, 1),
+    (7, '1998-01-23 12:45:56', '', 0.0, 1),
+    (8, '1998-01-23 12:45:56', '', 0.0, 1),
+    (9, '1998-01-23 12:45:56', '', 0.0, 1),
+    (10, '1998-01-23 12:45:56', '', 0.0, 1),
+    (11, '1998-01-23 12:45:56', '', 0.0, 1),
+    (12, '1998-01-23 12:45:56', '', 0.0, 1),
+    (13, '1998-01-23 12:45:56', '', 0.0, 1),
+    (14, '1998-01-23 12:45:56', '', 0.0, 1),
+    (15, '1998-01-23 12:45:56', '', 0.0, 1)
+;
